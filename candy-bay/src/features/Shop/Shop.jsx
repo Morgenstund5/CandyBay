7285b48ff12a313aec8580e17d2a8bab50a6d3d3
Morgenstund <emilianopesquedua@gmail.com>
2026-05-09 12:50:04 -0300

    Loading state, product-box animation & grid responsiveness

--- candy-bay/src/features/Shop/Shop.jsx
+++ candy-bay/src/features/Shop/Shop.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import purpleFlower from '../../assets/products/bg/purple-flower.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus, faCartShopping, faCheck } from '@fortawesome/free-solid-svg-icons'
@@ -14,6 +14,7 @@ function Shop() {
   const {priceBCH} = useContext(PriceContext)
   const { addToCart } = useContext(CartContext)
   const [added, setAdded] = useState()
+  const [isLoading, setIsLoading] = useState(true)
 
   const handleCart = (product) => {
     addToCart(product)
@@ -23,26 +24,38 @@ function Shop() {
     }, 2000)
   } 
 
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false)
+    },600)
+  })
+
   return (
     <div className='shop'>
-      {products.map(product => (
-          <Link className='product-box' key={product.id} to={`/product/${product.id}`}>
-            <h2>{product.title}</h2>
-            <span>{product.category}</span>
-            {/*<span>{product.category}</span>*/}
-            <div className="product-logo" style={{ backgroundImage: `url(${product.img})` }}></div>
-            {/*<img src={product.img} className='product-logo'/>*/}
-            <p>{displayRating(product.rating)}</p>
-            <p>{convertPrice(product.price, priceBCH)} BCH</p>
-            <div onClick={(e) => {
-              e.preventDefault(); 
-              e.stopPropagation()
-              handleCart(product)
-              }}>
-              <FontAwesomeIcon icon={added === product ? faCheck : faCartShopping} className='cart'/>
-            </div>
-        </Link>
-      ))}
+      {!isLoading ? (
+        products.map(product => (
+            <Link className='product-box' key={product.id} to={`/product/${product.id}`}>
+              <h2>{product.title}</h2>
+              <span>{product.category}</span>
+              {/*<span>{product.category}</span>*/}
+              <div className="product-logo" style={{ backgroundImage: `url(${product.img})` }}></div>
+              {/*<img src={product.img} className='product-logo'/>*/}
+              <p>{displayRating(product.rating)}</p>
+              <p>{convertPrice(product.price, priceBCH)} BCH</p>
+              <div onClick={(e) => {
+                e.preventDefault(); 
+                e.stopPropagation()
+                handleCart(product)
+                }}>
+                <FontAwesomeIcon icon={added === product ? faCheck : faCartShopping} className='cart'/>
+              </div>
+            </Link>
+        )))
+      : (
+        <div className="load-cont">
+          <span className="loader-shop"></span>
+        </div>
+    )}
     </div>
   )
 }
